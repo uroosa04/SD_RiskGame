a03f2508c2031adcb7e017507e0227c639044de1
Jie <caicoding2@gmail.com>
2018-09-13 15:42:06 -0500

    Make change in Game

--- Game.java
+++ Game.java
@@ -11,14 +11,17 @@ public class Game {
 		System.out.println("Welcome to RISK!");
 		System.out.println("How many players? (2, 3, 4, 5, or 6)");
 		numberOfPlayers = sc.nextInt();
-		while (numberOfPlayers != 2 && numberOfPlayers != 3) {
+		while (numberOfPlayers < 2 && numberOfPlayers > 6) {
 			System.out.println("Please input an appropiate number of players. (2, 3, 4, 5, or 6)");
-			numberOfPlayers = sc.nextInt();
+			numberOfPlayers = sc.nextInt() ;
 		}
 		
 		System.out.println("Got it, " + numberOfPlayers + " players.");
 		
-		System.out.println("What is Player 1's name?");
+		for (int i = 1; i <= numberOfPlayers; i++) {
+			System.out.println("What is Player "+ i + " name?");
+		}
+		sc.close();
 		
 	}
 	
