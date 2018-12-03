d26ccd51c5676c7ca819612f1b957c3f7fb04961
uroosa04 <uroosa.234@gmail.com>
2018-12-02 18:45:49 -0600

    Add CardTest

---
+++ src/test/java/Testing/CardTest.java
@@ -0,0 +1,22 @@
+package RSG;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import RSG.Card;
+import RSG.Country;
+public class CardTest {
+	Country testC = new Country("India");
+	Card test = new Card("Infantry", testC);
+	@Test
+	public void testGetCountry() {
+		assertEquals(testC, test.getCountry());
+	}
+
+	@Test
+	public void testGetType() {
+		assertEquals("Infantry", test.getType());
+	}
+
+
+}
